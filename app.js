bc534fd74eb13bd6ae5ea9cf957373bfd2c69e18
aysenurkartall <aysenur.kartal@std.yeditepe.edu.tr>
2024-10-16 13:44:17 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+// 1. If-else 
+let num = 7;
+
+if (num > 5) {
+    console.log("Sayı 5'ten büyüktür.");
+} else {
+    console.log("Sayı 5'ten küçüktür veya eşittir.");
+}
+
+// 2. Matematiksel işlem
+let a = 10;
+let b = 5;
+let sonuc = a * b;  // Çarpma operatörü
+console.log("Sonuç: " + sonuc);
+
+// 3. For döngüsü ile 1'den 10'a kadar sayıları yazdırma
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+}
